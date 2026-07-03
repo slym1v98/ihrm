34a53a5430a7405f442d39b13d03cc43db665b0c
thuyld <thuyhu9876@gmail.com>
2026-07-03 18:20:02 +0700

    feat(asset): add repository interfaces

---
+++ src/backend/app/Modules/Asset/Domain/Repositories/AssetReturnRepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Modules\Asset\Domain\Repositories;
+
+use App\Modules\Asset\Domain\Aggregates\AssetReturn\AssetReturn;
+use App\Modules\Asset\Domain\ValueObjects\AssetReturnId;
+
+interface AssetReturnRepositoryInterface
+{
+    public function findById(AssetReturnId $id): ?AssetReturn;
+    public function save(AssetReturn $return): void;
+}
