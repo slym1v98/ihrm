5fadd40c4d9bd68bf59ad5ef7c264eb4ec26adea
thuyld <thuyhu9876@gmail.com>
2026-07-05 23:34:43 +0700

    style: fix Pint

--- src/backend/routes/console.php
+++ src/backend/routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command("sanctum:prune-expired", ["--hours" => 24])->daily();
+Schedule::command('sanctum:prune-expired', ['--hours' => 24])->daily();
